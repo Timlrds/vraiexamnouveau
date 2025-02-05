a531771a9fe70a6a597ac0c02e0dfaf10567f7b1
Timao Gourdais <timaogourdais@MacBook-Pro-de-Timao.local>
2025-02-05 16:51:01 +0100

    2

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const clickButton = document.getElementById('clickButton');
+    
+    clickButton.addEventListener('click', function() {
+        alert("Clarisse c'est vraiment la plus belle ");
+    });
+}); 
